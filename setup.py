6952cf0b6e91e5680bbd0d962cda0a50c1052944
CLOVER-6 <89226564+CLOVER-6@users.noreply.github.com>
2021-12-26 12:31:50 +0000

    Update setup.py
    adding  comments, cos why not

--- setup.py
+++ setup.py
@@ -3,33 +3,40 @@ import sys
 import requests
 from pathlib import Path
 
-
+# set args for retrieving userkey
 devkey = sys.argv[1]
 usename = sys.argv[2]
 passwd = sys.argv[3]
 
+# replace every "<DEVKEY>" in sourcepaste with a user's actual API dev key
 file = Path('sourcepaste')
 to_file = Path("/usr/bin")
 
 file.write_text(file.read_text().replace('<DEVKEY>', devkey))
 
+# set post data to retrieve user
 data = {
 	"api_dev_key": devkey,
 	"api_user_name": usename,
 	"api_user_password": passwd
 }
 
+# set user-agent in case of blocking
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
     'Content-Type': 'application/x-www-form-urlencoded'
 }
 
+# retrieve and decode user's PasteBin API userkey
 apiget = requests.post("https://pastebin.com/api/api_login.php", headers=headers, data=data)
 
 userkey = apiget.content.decode("utf-8")
 
+# replace ever "<USERKEY>" in sourcepaste with a user's actual API user key
 file.write_text(file.read_text().replace('<USERKEY>', userkey))
 
+# copy sourcepaste to /usr/bin, for ease of use
 shutil.copy(file, to_file)
 
+# show user their userkey
 print(f"Your user key: {userkey}")
